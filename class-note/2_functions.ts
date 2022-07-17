785dbe5803a6c539f1941308d3f22a24bdb1b11a
정훈희/컨텐츠서비스개발팀/NE <hhjung@nhn.com>
2022-07-17 18:38:48 +0900

    함수타입 - 옵셔널 파라미터

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -2,3 +2,10 @@ function sum(a: number, b: number): number {
     return a + b;
 }
 sum(10, 20);
+
+function log(a: string, b?: string, c?: string) {
+
+}
+
+log('hello world');
+log('hello ts', 'abc')
